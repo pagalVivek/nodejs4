4b3d198a84a39e554a0ff8c4b125d185b360185b
pagalVivek <naruto567uzumaki009@gmail.com>
2023-12-29 16:36:20 +0530

    added setTimeout

---
+++ time.js
@@ -0,0 +1,16 @@
+// setTimeout(() => {
+//     console.log('hello1');
+// },2000)
+
+// console.log('hello2');
+
+
+setImmediate(() => {
+    console.log('Hello 1');
+})
+
+setTimeout(() => {
+    console.log('hell0 3');
+},2000)
+
+console.log('hello 2');
